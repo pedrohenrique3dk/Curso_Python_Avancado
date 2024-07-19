13f304174b89821a70e1085b2336578394d61626
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-07-18 23:09:52 -0300

    Python

---
+++ Python Básico/Aulas/Aula32.py
@@ -0,0 +1,17 @@
+"""
+Repetições
+while (enquanto)
+Execura uma aão enquanto uma condição for verdadeira
+loop infinito -> quando um cógigo não tem fim.
+
+"""
+
+#quando a condição é verdadeira, ele entra no while e quando falsa não entra no while.
+
+contador = 0
+
+while contador <= 10:
+        print(contador)
+        contador += 1
+        
+print('acabou')
